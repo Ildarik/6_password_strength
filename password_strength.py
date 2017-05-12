65465969837fcd91b6da45380b8800c99fe08bb2
Nogmanov Ildar <nogmanov@gmail.com>
2017-05-12 08:14:44 +0300

    Add variable result

--- password_strength.py
+++ password_strength.py
@@ -2,24 +2,26 @@ import getpass
 import re
 
 def get_password_strength(password):
+    result = 1
+
     length_error = len(password) < 8
     digit_error = re.search(r"\d", password) is None
     uppercase_error = re.search(r"[A-Z]", password) is None
     lowercase_error = re.search(r"[a-z]", password) is None
     symbol_error = re.search(r"\W", password) is None
+
+    if not length_error:
+        result += 1
+    if not digit_error:
+        result += 2
+    if not uppercase_error:
+        result += 2
+    if not lowercase_error:
+        result += 2
+    if not symbol_error:
+        result += 2
     
-    if length_error:
-    	return 1
-    elif lowercase_error:
-    	return 2
-    elif digit_error:
-        return 4	
-    elif symbol_error:
-        return 6
-    elif uppercase_error:
-    	return 8
-    else:
-    	return 10
+    return result
 
 
 if __name__ == '__main__':
